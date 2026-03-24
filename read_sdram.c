2702a52e5fa52d7dbf9a98fbaec37a67b7e431f9
David Eapen <davidjeapen@gmail.com>
2026-03-24 15:41:08 -0500

    Fixed issues

--- read_sdram.c
+++ read_sdram.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-#define SDRAM_START 0xC0000000
-#define SDRAM_END 0xC3FFFFFF
+#define SDRAM_BASE 0xC0000000
+#define SDRAM_SPAN 0x04000000 // 64 MB size (0xC3FFFFFF - 0xC0000000 + 1)
 #define NUM_READ_DATA 8
 
 int main() {
 	int mem_fd;
-	char* sdram_addr;
-
+	void *virtual_base;
+	
 	if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
 		printf("ERROR: could not open \"/dev/mem\"... (Are you running as root?)\n");
 	    return(1);
 	}
-	sdram_addr = (char *) mmap(NULL, SDRAM_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, SDRAM_BASE);
+
+	virtual_base = (char *) mmap(NULL, SDRAM_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, SDRAM_BASE);
+	volatile uint32_t *sdram_addr = (volatile uint32_t *)virtual_base;
 
 	for (int i = 0; i < NUM_READ_DATA; i++) {
 		printf("Val %d: %X", i, *(sdram_addr + i)); 
