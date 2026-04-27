c4483c5f6379ae8ae6067d8c1219f9c48d08424c
DE 1 <de1@554.bat>
2026-04-27 17:43:58 +0000

    test

--- read_tdoa.c
+++ read_tdoa.c
@@ -8,21 +8,21 @@
 // --- USER CONFIGURATION ---
 // Replace these with your actual FPGA memory base address and size
 #define FPGA_MEM_BASE_ADDR 0x30000000 // Example physical address
-#define DATA_SIZE_BYTES    7680000      // Amount of memory to read (e.g., 8KB)
+#define DATA_SIZE_BYTES    8388608      // Amount of memory to read (e.g., 8KB)
 #define NUM_SAMPLES        (DATA_SIZE_BYTES / sizeof(pcm_payload_t))
 
 // This struct perfectly matches your SystemVerilog 128-bit pcm_buffer.
 // ARM is little-endian, meaning the bits [15:0] from your SV code end 
 // up first in memory, and [127:112] end up last.
 typedef struct {
-    int16_t sta_1; // SV bits [15:0]
-    int16_t lta_1; // SV bits [31:16]
-    int16_t sta_2; // SV bits [47:32]
-    int16_t lta_2; // SV bits [63:48]
-    int16_t sta_3; // SV bits [79:64]
-    int16_t lta_3; // SV bits [95:80]
-    int16_t sta_4; // SV bits [111:96]
-    int16_t lta_4; // SV bits [127:112]
+    uint16_t hit_time_1; // SV bits [15:0]
+    uint16_t hit_time_2; // SV bits [31:16]
+    uint16_t hit_time_3; // SV bits [47:32]
+    uint16_t hit_time_4; // SV bits [63:48]
+    int16_t pcm_pos_1; // SV bits [79:64]
+    int16_t pcm_neg_1; // SV bits [95:80]
+    int16_t pcm_pos_2; // SV bits [111:96]
+    int16_t pcm_neg_2; // SV bits [127:112]
 } __attribute__((packed)) pcm_payload_t;
 
 int main() {
@@ -59,14 +59,14 @@ int main() {
     }
 
     // Write CSV Header
-    fprintf(out_fp, "sta_1, lta_1, sta_2, lta_2, sta_3, lta_3, sta_4, lta_4 \n");
+    fprintf(out_fp, "hit1,hit2,hit3,hit4,p1,n1,p2,n2\n");
 
     // 4. Read memory and interleave T1 and T2 dynamically
     printf("Reading %d hardware payloads from FPGA memory...\n", NUM_SAMPLES);
     for (int i = 0; i < NUM_SAMPLES; i++) {
         // Read the T1 (first) sample set from the current 128-bit payload
         fprintf(out_fp, "%d,%d,%d,%d,%d,%d,%d,%d\n",
-                buffer[i].sta_1, buffer[i].lta_1, buffer[i].sta_2, buffer[i].lta_2, buffer[i].sta_3, buffer[i].lta_3, buffer[i].sta_4, buffer[i].lta_4);
+                buffer[i].hit_time_1, buffer[i].hit_time_2, buffer[i].hit_time_3, buffer[i].hit_time_4, buffer[i].pcm_pos_1, buffer[i].pcm_neg_1, buffer[i].pcm_pos_2, buffer[i].pcm_neg_2);
 
     }
 
