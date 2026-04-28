a184b1c08d63c4d75b4ac3d3f60ddbc7c79d49ea
kmkraft2 <kmkraft2@wisc.edu>
2026-04-28 02:18:20 -0500

    Fixed printing

--- Overlays/tdoa_loc_overlay.cpp
+++ Overlays/tdoa_loc_overlay.cpp
@@ -125,7 +125,7 @@ static bool read_mic_delays(HwInterface &hw, uint16_t mic_delay[4])
     }
 
     bytes[byte_idx++] = byte;
-    std::cout << byte << "\n";
+    std::cout << std::hex << std::setfill(0) << std::setw(2) << (int)byte << "\n";
 
     if (byte_idx < 8) {
         return false;
